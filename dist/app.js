2b2510835c22d24129a10f7b7ab4e7125b28ba5b
Alejandro Pozo Castro <97757809+kpawn92@users.noreply.github.com>
2023-03-06 12:00:08 -0500

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -14,7 +14,7 @@ const swaggerOptions_1 = require("./swaggerOptions");
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
 const video_routes_1 = __importDefault(require("./routes/video.routes"));
-const initialSetup_1 = require("./libs/initialSetup");
+//const initialSetup_1 = require("./libs/initialSetup");
 const app = (0, express_1.default)();
 //middlewares
 app.use((0, cors_1.default)());
@@ -24,8 +24,8 @@ app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
 const specs = (0, swagger_jsdoc_1.default)(swaggerOptions_1.options);
 // Initial Setup
-(0, initialSetup_1.createRoles)();
-(0, initialSetup_1.accountAdmin)();
+//(0, initialSetup_1.createRoles)();
+//(0, initialSetup_1.accountAdmin)();
 // routes
 app.use('/auth/', auth_routes_1.default);
 app.use('/user/', user_routes_1.default);
